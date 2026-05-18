0ab38e97136361022d2170055751c53239747b6d
baranyagiz-kodlayici <mardinlibaran07@gmail.com>
2026-05-18 18:07:14 +0300

    Initial Yagiz Midye project setup

---
+++ src/main/java/com/yagizmidye/YagizMidyeApplication.java
@@ -0,0 +1,13 @@
+package com.yagizmidye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YagizMidyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YagizMidyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yagizmidye/YagizMidyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagizmidye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YagizMidyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
