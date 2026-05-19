5fc8b5a53f04c29471ce18a9ee83985818464743
baranyagiz-kodlayici <mardinlibaran07@gmail.com>
2026-05-19 14:02:17 +0300

    Add landing page

--- src/main/java/com/yagizmidye/controller/HomeController.java
+++ src/main/java/com/yagizmidye/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.yagizmidye.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,27 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Authentication authentication) {
-
-        if (authentication != null && authentication.isAuthenticated()) {
-
-            boolean isAdmin = authentication.getAuthorities()
-                    .stream()
-                    .anyMatch(role -> role.getAuthority().equals("ROLE_ADMIN"));
-
-            boolean isWaiter = authentication.getAuthorities()
-                    .stream()
-                    .anyMatch(role -> role.getAuthority().equals("ROLE_WAITER"));
-
-            if (isAdmin) {
-                return "redirect:/admin/products";
-            }
-
-            if (isWaiter) {
-                return "redirect:/waiter/orders";
-            }
-        }
-
-        return "redirect:/login";
+    public String home() {
+        return "index";
     }
 }
